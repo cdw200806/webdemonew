7ce6ca8c258a9e3e471370852281f4fb478fc7af
cdw200806 <924673193@qq.com>
2025-03-13 02:13:59 +0800

    Initial commit

---
+++ src/main/java/com/example/demowebnew/DemowebnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowebnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demowebnew/DemowebnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demowebnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
